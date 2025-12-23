517699ba14d3b62a441c7b8c8a47b8d1c556f206
Param2603 <ps26032005@gmail.com>
2025-12-23 15:50:56 +0530

    DSA

---
+++ Day1/practice.cpp
@@ -0,0 +1,16 @@
+// // # include <iostream>  //iostream = input/output stream also called header file or library file, # = pre-processor
+// // using namespace std; // namespace = create divison , std = standard, std namespace contain built-in class/object as cout & declared function    
+
+// // int main() {
+// //     cout << "Hello World";   // << = insertion operator , cout = works as print
+// //     return 0;
+// // }
+
+// // OR without using namespace 
+
+// # include <iostream>
+
+// int main() {
+//     std :: cout << "Welcome to the World";
+//     return 0;
+// }
