ab43dd4bf41146365dc734cd66211c583eb612de
Param2603 <ps26032005@gmail.com>
2026-01-20 15:32:11 +0530

    dsa

---
+++ N Day1/encapsulation.cpp
@@ -0,0 +1,14 @@
+// Q) What is encapsulation?
+// A. Encapsulation means combining data and the functions that work on that data into a single unit, like      
+//    a class.
+// we use the encapsulation and use getter (getName) and setter (setName) method which are used to show and modify the private data.
+
+#include<iostream>
+using namespace std;
+
+class Encap {
+    private:          // access not given if getter and setter is not there
+         
+
+    public:           // default is also public it access
+};
