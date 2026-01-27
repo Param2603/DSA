5edc4efb6c4f549488b372949be5f520dddbf76a
Param2603 <ps26032005@gmail.com>
2026-01-27 16:14:10 +0530

    sort

--- sort/binarySearch.cpp
+++ sort/binarySearch.cpp
@@ -1,12 +1,29 @@
 #include<iostream>
 using namespace std;
 
+int sort(int arr[], int n){
+        for(int i=0; i<n-1; i++){
+            for(int j=0; j<n-i-1; j++){
+
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+
+        } return arr[n];
+    }
+
 int main(){
     int n, key;
-    int arr[100];
+
 
     cout<<"Enter number of elements:";
     cin>>n;
+
+    int arr[n];
+
 
     cout<<"Enter elements";
     for(int i=0; i<n; i++){
@@ -15,6 +32,9 @@ int main(){
 
     cout<<"Enter element to Search: "<<endl;
     cin>>key;
+
+    sort(arr, n);
+    cout<<"Sorted Array"<<endl;
 
     int start = 0;
     int end = n-1;
