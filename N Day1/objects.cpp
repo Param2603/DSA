3c02f893254026c24d7563c1ba528c73896010a7
Param2603 <ps26032005@gmail.com>
2026-02-02 14:03:04 +0530

    dsa

--- N Day1/objects.cpp
+++ N Day1/objects.cpp
@@ -1,3 +1,33 @@
 #include<iostream>
 using namespace std;
 
+class Student {
+    public:
+    int roll;
+    string name;
+
+    void getData(){
+        cout<<"Enter Name: "<<endl;
+        cin>>name;
+
+        cout<<"Enter Roll_Number: "<<endl;
+        cin>>roll;
+    }
+
+    void showData(){
+        cout<<"Name"<<name<<endl;
+        cout<<"Roll_Number"<<roll<<endl;
+    }
+};
+
+int main(){
+    Student s[3];
+
+       for(int i = 0; i < 3; i++) {
+        s[i].getData();
+    }
+   
+    for(int i = 0; i < 3; i++) {
+        s[i].showData();
+    }
+}
