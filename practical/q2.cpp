52fe3f2d0544d5a51ac12030ad776038b528aee1
Param2603 <ps26032005@gmail.com>
2026-03-26 21:47:26 +0530

    dsa_practical

--- practical/q2.cpp
+++ practical/q2.cpp
@@ -2,3 +2,25 @@
 
 #include<iostream>
 using namespace std;
+
+int main(){
+    int arr[] = {1, 2, 2, 3, 1, 4, 2};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    for(int i = 0; i < n; i++){
+        int count = 1;
+
+        if(arr[i] == -1) { 
+            continue;
+        } // skip counted elements
+
+        for(int j = i + 1; j < n; j++){
+            if(arr[i] == arr[j]){
+                count++;
+                arr[j] = -1; // mark as counted
+            }
+        }
+
+        cout << arr[i] << " => " << count << endl;
+    }
+}
